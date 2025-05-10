f22f905712b3da7e7208507bc542f431a6cbe384
Gabriel Gallardo R.(Doe) <gabriel.egallardor@gmail.com>
2025-05-10 19:27:13 -0400

    FEAT(Coin): Adding base for Coin class

---
+++ structure/Coin.py
@@ -0,0 +1,11 @@
+class Coin:
+    def __init__(self, mental: int, physical: int):
+        if not isinstance(mental, int) or not 1 <= mental <= 5:
+            raise ValueError("El atributo 'mental' debe ser un entero entre 1 y 5")
+        if not isinstance(physical, int) or not 1 <= physical <= 5:
+            raise ValueError("El atributo 'physical' debe ser un entero entre 1 y 5")
+
+        self.mental = mental
+        self.physical = physical
+        
+    def __str__(self): return f"Coin(mental={self.mental}, physical={self.physical})"
